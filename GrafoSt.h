683e394beacca70872b2cccc0846b61721404f7d
Victor Martini <vmartini@iMac.local>
2021-03-24 20:48:30 -0300

    Agregada definicion tentativa de GrafoSt

--- GrafoSt.h
+++ GrafoSt.h
@@ -3,7 +3,17 @@
 typedef uint32_t u32;
 
 struct GrafoSt{
-  
+  u32 num_vertices;
+  u32 num_lados;
+  u32 delta;
+  Nodo* nodos;
 
 
+};
+
+struct Nodo{
+    u32 nombre;
+    u32 color;
+    u32 grado;
+    Nodo* vecinos;
 };
