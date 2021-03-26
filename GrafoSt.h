11749489b4316f8e919bd4afa73c780f99c5ebc2
VicMartini <victor.cesar.m@gmail.com>
2021-03-25 23:22:12 -0300

    Agregada la estructura Lado y reemplazadas las ocurrencias de 'nodo' por 'vertice'

--- GrafoSt.h
+++ GrafoSt.h
@@ -2,31 +2,39 @@
 
 typedef uint32_t u32;
 
-/*El grafo contiene los campos que necesitamos en  O(1) y un arreglo de nodos.
-Si usamos un array de punteros a nodos eliminamos la necesidad de usar un array para el orden
-Como los nodos estan en un array, acceder 
-a un nodo teniendo su número de orden en orden inicial también es O(1). */
+/*El grafo contiene los campos que necesitamos en  O(1) y un arreglo de vertices.
+Si usamos un array de punteros a Vertices eliminamos la necesidad de usar un array para el orden
+Como los vertices estan en un array, acceder 
+a un vertice teniendo su número de orden en orden inicial también es O(1). */
 
-struct GrafoSt{
+struct GrafoSt
+{
   u32 num_vertices;
   u32 num_lados;
   u32 delta;
-  Nodo** nodos; /*Esto va a ser un array de punteros nodos, como la cantidad de nodos está al principio del archivo dimacs, ya sabemos
+  Vertice **Vertices; /*Esto va a ser un array de punteros a vertices, como la cantidad de vertices está al principio del archivo dimacs, ya sabemos
   desde el comienzo cuanto espacio reservar para el array*/
-
-
 };
-/*Los nodos contienen la información que necesitamos en O(1) + una linked list  que tiene punteros a sus vecinos 
+
+struct Lado
+{
+  u32 v;
+  u32 w;
+};
+
+/*Los vertices contienen la información que necesitamos en O(1) + una linked list  que tiene punteros a sus vecinos 
 Todo es O(1) salvo buscar un vecino que O(n) en el peor de los casos*/
-struct Nodo{
-    u32 nombre;
-    u32 color;
-    u32 grado;
-    ListaNodos* vecinos; // Esto necesita ser una Linked List porque no sabemos el grado del nodo hasta terminar de construir el grafo.
+struct Vertice
+{
+  u32 nombre;
+  u32 color;
+  u32 grado;
+  ListaVertices *vecinos; // Esto necesita ser una Linked List porque no sabemos el grado del vertice hasta terminar de construir el grafo.
 };
 
 // Struct para la linked list.
-struct ListaNodos{ 
-    Nodo* nodo;
-    ListaNodos* prox;
+struct ListaVertices
+{
+  Vertice *Vertice;
+  ListaVertices *prox;
 };
