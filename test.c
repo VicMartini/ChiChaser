f545771f32eede84d0b135cce38c57927c0de06a
juanfvega <juanfedericovega@gmail.com>
2021-03-25 21:23:31 -0300

    add parse edges

--- test.c
+++ test.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
 /*
  compilar gcc test.c
  ./a.out < dimacs 
@@ -8,7 +9,7 @@
 */ 
 char* parse_correct_form_edge(char *str);
 int parse_p_edge_n_m(void);
-
+int parse_edge(void);
 /**
  * @brief chequea que la palabra este en forma correcta
  *         
@@ -34,12 +35,14 @@ int parse_p_edge_n_m(void){
 char *readString = NULL; 
 unsigned int strToULong;
 char *ptr = NULL;
-
+bool flag = false;
 
 readString = (char *)calloc(1, sizeof(char));
 
-  while (fgets(readString, 1024, stdin)!= NULL) {
-    if(readString[0]=='p'){
+  while (!flag) {
+    if(fgets(readString, 1024, stdin) == NULL)
+      return -1;
+    if(readString[0] == 'p'){
       if ((readString = parse_correct_form_edge(readString)) == NULL){
         return -1;
       }
@@ -48,14 +51,45 @@ readString = (char *)calloc(1, sizeof(char));
       readString = strtok(NULL, " ");
       strToULong = strtoul(readString, &ptr, 10);
       printf("2-%d\n", strToULong);
+      flag = true;
     }
   }
 
 return 0;
 }
 
+
+int parse_edge(void){
+  bool flag = false;
+  char *readStr = NULL;
+  char *token, *ptr = NULL;
+  unsigned int a, b = 0;
+  
+  readStr = (char *) calloc(1, sizeof(char));
+
+  for (int i = 0; i < 20; i++){
+    if(fgets(readStr, 1024, stdin) == NULL)
+      return -1;
+
+    if(readStr[0] = 'e'){
+      token = strtok(readStr, "e ");
+      a = (unsigned int) strtoul(token, &ptr, 10);
+      token = strtok(NULL, " ");
+      b = (unsigned int) strtoul(token, &ptr, 10);
+      printf("vertice A: %d - Vertice B: %d \n", a, b);
+      
+
+    }
+
+
+  }
+return 0;
+}
+
+
 int main(int argc, char ** argv){
 
   parse_p_edge_n_m();
+  parse_edge();
   return 0;
 }
