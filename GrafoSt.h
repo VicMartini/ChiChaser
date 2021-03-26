5753395f757649338082635b918c3e09045f435e
juanfvega <juanfedericovega@gmail.com>
2021-03-25 22:56:38 -0300

    se agrega parser que devuelve array de vertices

--- GrafoSt.h
+++ GrafoSt.h
@@ -1,21 +1,24 @@
 #include <stdint.h>
 
 typedef uint32_t u32;
+typedef struct Lado Lado_st;
 
 /*El grafo contiene los campos que necesitamos en  O(1) y un arreglo de vertices.
 Si usamos un array de punteros a Vertices eliminamos la necesidad de usar un array para el orden
 Como los vertices estan en un array, acceder 
 a un vertice teniendo su número de orden en orden inicial también es O(1). */
 
+/*
 struct GrafoSt
 {
   u32 num_vertices;
   u32 num_lados;
   u32 delta;
   Vertice **Vertices; /*Esto va a ser un array de punteros a vertices, como la cantidad de vertices está al principio del archivo dimacs, ya sabemos
-  desde el comienzo cuanto espacio reservar para el array*/
+  desde el comienzo cuanto espacio reservar para el array
 };
 
+*/
 struct Lado
 {
   u32 v;
@@ -23,7 +26,7 @@ struct Lado
 };
 
 /*Los vertices contienen la información que necesitamos en O(1) + una linked list  que tiene punteros a sus vecinos 
-Todo es O(1) salvo buscar un vecino que O(n) en el peor de los casos*/
+Todo es O(1) salvo buscar un vecino que O(n) en el peor de los casos*
 struct Vertice
 {
   u32 nombre;
@@ -38,3 +41,4 @@ struct ListaVertices
   Vertice *Vertice;
   ListaVertices *prox;
 };
+*/
--- test.c
+++ test.c
@@ -2,14 +2,16 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdbool.h>
+#include "GrafoSt.h"
+
 /*
  compilar gcc test.c
  ./a.out < dimacs 
 
 */ 
 char* parse_correct_form_edge(char *str);
-int parse_p_edge_n_m(void);
-int parse_edge(void);
+Lado_st* parse_p_edge_n_m(void);
+Lado_st** parse_edge(Lado_st *lados);
 /**
  * @brief chequea que la palabra este en forma correcta
  *         
@@ -31,65 +33,86 @@ char* parse_correct_form_edge(char *str){
 }
 
 
-int parse_p_edge_n_m(void){
+Lado_st* parse_p_edge_n_m(void){
 char *readString = NULL; 
-unsigned int strToULong;
+u32 strToULong;
 char *ptr = NULL;
 bool flag = false;
+Lado_st *Data = NULL;
 
+Data = (Lado_st*) calloc(1, sizeof(struct Lado));
 readString = (char *)calloc(1, sizeof(char));
 
   while (!flag) {
     if(fgets(readString, 1024, stdin) == NULL)
-      return -1;
+      return NULL;
     if(readString[0] == 'p'){
       if ((readString = parse_correct_form_edge(readString)) == NULL){
-        return -1;
+        return NULL;
       }
       strToULong = strtoul(readString, &ptr, 10);
       printf("1-%d\n", strToULong);
+      Data->v = strToULong;
       readString = strtok(NULL, " ");
       strToULong = strtoul(readString, &ptr, 10);
+      Data->w = strToULong;
       printf("2-%d\n", strToULong);
       flag = true;
     }
   }
 
-return 0;
+return Data;
 }
 
 
-int parse_edge(void){
+Lado_st** parse_edge(Lado_st *lados){
   bool flag = false;
-  char *readStr = NULL;
-  char *token, *ptr = NULL;
-  unsigned int a, b = 0;
+  char *token, *ptr ,*readStr = NULL;
+  u32 a, b, M = 0;
+  Lado_st **array_lados = NULL;
   
+  M = lados->w;
   readStr = (char *) calloc(1, sizeof(char));
+  array_lados = (Lado_st **) calloc(M, sizeof(Lado_st));
+  for (int i = 0; i < M; i++)
+  {
+    array_lados[i] = calloc(1, sizeof(struct Lado));
+  }
+  
 
-  for (int i = 0; i < 20; i++){
+  for (int i = 0; i < M; i++){
     if(fgets(readStr, 1024, stdin) == NULL)
-      return -1;
-
+      return NULL;
     if(readStr[0] = 'e'){
       token = strtok(readStr, "e ");
       a = (unsigned int) strtoul(token, &ptr, 10);
       token = strtok(NULL, " ");
       b = (unsigned int) strtoul(token, &ptr, 10);
-      printf("vertice A: %d - Vertice B: %d \n", a, b);
-      
+      array_lados[i]->v = a;
+      array_lados[i]->w = b;
 
     }
 
 
   }
-return 0;
+return array_lados;
 }
 
 
 int main(int argc, char ** argv){
+  Lado_st *infoEdge = NULL;
+  Lado_st **array = NULL;
+  u32 M = 0;
+  infoEdge = parse_p_edge_n_m();
+  M = infoEdge->w;
+  array = parse_edge(infoEdge);
 
-  parse_p_edge_n_m();
-  parse_edge();
+  /*test array */
+
+  for (int i = 0; i < M; i++)
+  {
+    printf("array[%d] : a=%d - b=%d  \n", i, array[i]->v, array[i]->w);
+  }
+  
   return 0;
 }
