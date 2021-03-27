8ff941443636c3fe6c39001732bee3af28eed75a
Victor Martini <vmartini@iMac.local>
2021-03-27 16:24:37 -0300

    Agregada función para crear una hash_list vacia

--- HashList.c
+++ HashList.c
@@ -8,5 +8,6 @@ hash_list new_hash_list(int size)
     hash_list new_hash_list = malloc(sizeof(struct hash_list_s));
     new_hash_list -> size = size;
     new_hash_list -> ocupation = 0;
-    new_hash_list -> entries = calloc(size, sizeof())
+    new_hash_list -> buckets = calloc(size, sizeof(list)); //Quizás deberiamos agregar un loop para inicializar
+                                                            //todas las posiciones de la tabla a NULL
 };
