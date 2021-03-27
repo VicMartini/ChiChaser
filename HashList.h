e9fd09c5db66fd7b857c1ef3924562b01d16024d
Victor Martini <vmartini@iMac.local>
2021-03-27 16:13:58 -0300

    Añadidos los headers de las funciones básicas de la HashList

--- HashList.h
+++ HashList.h
@@ -1,29 +1,27 @@
-#include <stdint.h>
 #include "GrafoSt.h"
+#include "List.h"
 typedef uint32_t u32;
-/* Esta estructura de datos es una implementación de una lista de adyacencia usando hash tables*/
+/* Esta estructura de datos es una implementación de una lista de adyacencia usando hash tables.
+Usar la hashtable nos permite hacer frente al problema de que los nodos del grafos no van a ser
+necesariamente enteros contiguos, lo cual hace inviable usar la implementación tradicional de una
+lista de adyacencia.*/
 
-typedef struct hash_list_s{
-    int size;
-    entry **entries;
-} hash_list;
+#define ENTRY_TYPE vertice;
 
-typedef struct entry_s{
-    u32 *key;
-    list *vertice; //Lista enlazada de vertices que tienen el mismo hash
-} entry;
-
-typedef struct list_s{ //Vamos a usar la lista para guardar los vecinos de un vertice y también para  manejar colisiones en la hashtable.
-    vertice* vertice;
-    list* next;
-} list;
-
-/*Los vertices contienen la información que necesitamos en O(1) + una linked list  que tiene punteros a sus vecinos 
-Todo es O(1) salvo buscar un vecino que O(n) en el peor de los casos*/
-typedef struct vertice_st
+//---------------Estructuras---------------------------
+struct hash_list_s
 {
-  u32 nombre;
-  u32 color;
-  u32 grado;
-  list *vecinos; // Esto necesita ser una Linked List porque no sabemos el grado del vertice hasta terminar de construir el grafo.
-} vertice;
+    int size;
+    int ocupation;
+    list *buckets; //Cada bucket es una lista de los vertices con el mismo hash.
+};
+    
+typedef struct hash_list_s *hash_list;
+
+//---------------Métodos---------------------------
+
+hash_list new_hl(int size);              //Crea una hashlist vacia dado su tamaño.
+hash_list add_hl_entry(vertice *entry);  // Añade unn vertice a la hashtable
+hash_list lookup_hl_entry(u32 key);      //Busca un vertice en la hashtable
+u32 hash_function(vertice *nombre_real); // Devuelve el hash para un vertice
+                                         //Quizás podriamos pasarle solo el nombre real
