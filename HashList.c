e0dab796906042724d3dd9eb0530e8fe43767264
Victor Martini <vmartini@iMac.local>
2021-03-27 16:38:45 -0300

    Añadida una hash function muy sencilla

--- HashList.c
+++ HashList.c
@@ -3,7 +3,7 @@
 #include <stdlib.h>
 #include "HashList.h"
 
-hash_list new_hash_list(int size)
+hash_list new_hl(int size)
 {
     hash_list new_hash_list = malloc(sizeof(struct hash_list_s));
     new_hash_list -> size = size;
@@ -11,3 +11,7 @@ hash_list new_hash_list(int size)
     new_hash_list -> buckets = calloc(size, sizeof(list)); //Quizás deberiamos agregar un loop para inicializar
                                                             //todas las posiciones de la tabla a NULL
 };
+
+u32 hash(u32 nombre_real, hash_list hl){
+    return nombre_real % hl -> size; //Un hash modular muy sencillo que deberia servirnos por el momento
+};                                     //deberiamos ver cómo se comporta y decidir si necesitamos algo más sofisticado.
--- HashList.h
+++ HashList.h
@@ -1,5 +1,6 @@
 #include "GrafoSt.h"
 #include "List.h"
+#include "VerticeSt.h"
 typedef uint32_t u32;
 /* Esta estructura de datos es una implementación de una lista de adyacencia usando hash tables.
 Usar la hashtable nos permite hacer frente al problema de que los nodos del grafos no van a ser
@@ -15,7 +16,7 @@ struct hash_list_s
     int ocupation;
     list *buckets; //Cada bucket es una lista de los vertices con el mismo hash.
 };
-    
+
 typedef struct hash_list_s *hash_list;
 
 //---------------Métodos---------------------------
@@ -23,5 +24,4 @@ typedef struct hash_list_s *hash_list;
 hash_list new_hl(int size);              //Crea una hashlist vacia dado su tamaño.
 hash_list add_hl_entry(vertice *entry);  // Añade unn vertice a la hashtable
 hash_list lookup_hl_entry(u32 key);      //Busca un vertice en la hashtable
-u32 hash_function(vertice *nombre_real); // Devuelve el hash para un vertice
-                                         //Quizás podriamos pasarle solo el nombre real
+u32 hash(u32 nombre_real, hash_list hl); // Devuelve el hash para un vertice y una hashlist en particular.
