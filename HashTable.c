7c552c5682889c3dc6df73d5425301238175bc09
Victor Martini <vmartini@iMac.local>
2021-03-30 18:32:48 -0300

    Cambios mayores en la hashtable: renombradas funciones y agregado array para iteración eficiente

--- HashTable.c
+++ HashTable.c
@@ -3,39 +3,53 @@
 #include <stdlib.h>
 #include "HashTable.h"
 
-
-u32 hash(u32 nombre_real, hash_table ht){
-    return nombre_real % ht -> size; //Un hash modular muy sencillo que deberia servirnos por el momento
-};                                     //deberiamos ver cómo se comporta y decidir si necesitamos algo más sofisticado.
+u32 hash(u32 nombre_real, hash_table ht)
+{
+    return nombre_real % ht->size; //Un hash modular muy sencillo que deberia servirnos por el momento
+};                                 //deberiamos ver cómo se comporta y decidir si necesitamos algo más sofisticado.
 
 hash_table new_ht(int size)
 {
     hash_table new_ht = malloc(sizeof(struct hash_table_s));
-    new_ht -> size = size;
-    new_ht -> ocupation = 0;
-    new_ht-> buckets = calloc(size, sizeof(list)); 
+    new_ht->size = size;
+    new_ht->ocupation = 0;
+    new_ht->order = calloc(size, sizeof(vertice *));
+    new_ht->buckets = calloc(size, sizeof(list));
     for (u32 i = 0; i < size; ++i)
     {
-        new_ht -> buckets[i] = new_list();
-    }       
-    return new_ht;                                                 
+        new_ht->buckets[i] = new_list();
+    }
+    return new_ht;
 };
 
-void add_ht_entry(u32 key, hash_table ht)
+bool in_ht(u32 key, hash_table ht)
+{
+    return in_list(key, ht->buckets[hash(key, ht)]);
+}
+
+vertice *ht_put(u32 key, hash_table ht) // Si el vertice no está lo agregamos y devolvemos un puntero hacia él. Si el vertice está funciona como get.
 {
     //Primero hasheamos el nombre real para ver a cual bucket hay que agregar el vertice
     u32 hsh = hash(key, ht);
-    // Y con esto ya podemos agregar el vertice al bucket correspondiente usando la interfaz de list
-    // gracias a addr_idemp podemos agregar sin crear duplicados, osea esta función también es idempotente
-    ht -> buckets[hsh] = addr_idemp(key, ht -> buckets[hsh]);
+    // Verificamos si el vertice está en la hashtable
+    vertice *v = search(key, ht->buckets[hsh]); //Esto es NULL si el vertice no está
+    if (!v)                                     //No está el vertice vamos a agregarlo a la tabla y al array de orden.
+    {
+        ht->buckets[hsh] = addl(key, ht->buckets[hsh]);    //Ahora el nuevo vertice es el primero en el bucket
+        ht->order[ht->ocupation] = head(ht->buckets[hsh]); // Agregamos el puntero al nuevo vertice a la lista en la posición
+        ht->ocupation += 1;
+        return head(ht->buckets[hsh]);                    // que corresponde a su orden de cargado.
+    }
+    else // El vertice ya estaba, devolvemos el puntero hacia él.
+    {
+        return v;
+    }
     // Me parece que quedó mucho más simple que si hubieramos usado open addressing :)
 };
 
-vertice *lookup_ht_entry(u32 key, hash_table ht)
+vertice *ht_get(u32 key, hash_table ht)
 {
-    vertice *v;
-    //Muy parecido a la anterior, primero hasheamos la key
     u32 hsh = hash(key, ht);
     /*Gracias al nuevo metodo search de la lista solo tenemos que hacer una llamada acá*/
-    return search(key, ht -> buckets[hsh]);
+    return search(key, ht->buckets[hsh]);
 };
--- HashTable.h
+++ HashTable.h
@@ -11,6 +11,8 @@ struct hash_table_s
 {
     int size;
     int ocupation;
+    vertice** order; //Un array de punteros a vertices que podemos usar para iterar eficientemente sobre todos los elementos de la hashtable
+                    //inspirado en la LinkedHashMap de Java.
     list *buckets; //Cada bucket es una lista de los vertices con el mismo hash.
 };
 
@@ -19,6 +21,6 @@ typedef struct hash_table_s *hash_table;
 //---------------Métodos---------------------------
 
 hash_table new_ht(int size);              //Crea una hashtist vacia dado su tamaño.
-void add_ht_entry(u32 key, hash_table ht);  // Añade un vertice a la hashtable, si el vertice no esta ya en la tabla.
-vertice* lookup_ht_entry(u32 key, hash_table ht);      //Busca un vertice en la hashtable
+vertice* ht_put(u32 key, hash_table ht);  // Añade un vertice a la hashtable, si el vertice no esta ya en la tabla.
+vertice* ht_get(u32 key, hash_table ht);      //Busca un vertice en la hashtable
 u32 hash(u32 nombre_real, hash_table ht); // Devuelve el hash para un vertice y una hashtist en particular.
