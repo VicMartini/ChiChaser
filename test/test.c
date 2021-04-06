7d2baa2f42ae6eb5c70ef9858a044ee1fd08dfcb
juanfvega <juanfedericovega@gmail.com>
2021-04-06 18:02:31 -0300

    separando tests

--- test/test.c
+++ test/test.c
@@ -57,48 +57,39 @@ test_list_head(const MunitParameter params[], void* fixture) {
 
 static MunitResult
 test_list_search(const MunitParameter params[], void* fixture) {
-  list q = (list) fixture;
-  vertice * ver;
+   list q = (list) fixture;
+  vertice *v = NULL;
   // agrego 3 elementos 
-  q = addr_idemp(1,q);
-  q = addr_idemp(2,q);
-  q = addr_idemp(3,q);
+  q = addl(1, v, q);
+
+  q = addl(2, v, q);
+
+  q = addl(3, v, q);
   // busco un el 2 
-  ver = search(2, q);
-  // check que devuelve 
-  munit_assert_uint32(ver->nombre, == , 2);
+  v = search(2, q);
+
+  munit_assert_uint32(v->nombre, == , 2);
+  // check que devuelve  */
 
   return MUNIT_OK;
 }
 
 static MunitResult
-test_list_search_false(const MunitParameter params[], void* fixture) {
+test_search_not_in_list(const MunitParameter params[], void* fixture) {
   list q = (list) fixture;
-  vertice * ver;
+  vertice * v;
   // agrego 3 elementos 
-  q = addr_idemp(1,q);
-  q = addr_idemp(2,q);
-  q = addr_idemp(3,q);
+  q = addl(1, v, q);
+  q = addl(2, v, q);
+  q = addl(3, v, q);
   // busco el valor 4 , debe devolver null
-  ver = search(4, q);
+  v = search(4, q);
 
-  munit_assert_ptr_null(ver);
+  munit_assert_ptr_null(v);
 
   return MUNIT_OK;
 }
 
-static MunitResult
-test_search_in_list(const MunitParameter params[], void* fixture) {
-  list q = (list) fixture;
-  vertice * ver;
-  // agrego 3 elementos 
-  q = addr_idemp(1, q);
-  q = addr_idemp(2, q);
-  q = addr_idemp(3, q);
-  //check que 4 no esta en la lista , debe ser falso
-  munit_assert_false(in_list(4, q));
-  return MUNIT_OK;
-}
 /* parametros , cuando se hace un test y se necesita parametros 
    se crea una arreglo y se pasa a MunitParameterEnum 
    luego en la suite se agrega el nombre como parametro en este caso file_params
@@ -246,18 +237,12 @@ test_hash_tear_down(void* fixture) {
 
 /* Creating a test suite is pretty simple.  First, you'll need an
  * array of tests: */
-static MunitTest test_suite_tests[] = {
+static MunitTest test_list[] = {
   {
     (char*) "/test_is_null",
     test_list_is_null,
     test_list_setup,
-    /* If you passed a callback for the fixture setup function, you
-     * may want to pass a corresponding callback here to reverse the
-     * operation. */
     test_list_tear_down,
-    /* Finally, there is a bitmask for options you can pass here.  You
-     * can provide either MUNIT_TEST_OPTION_NONE or 0 here to use the
-     * defaults. */
     MUNIT_TEST_OPTION_NONE,
     NULL
   },
@@ -278,21 +263,29 @@ static MunitTest test_suite_tests[] = {
     NULL
   },
   {
-    (char*) "/test_search_false",
-    test_list_search_false,
+    (char*) "/test_list_search",
+    test_list_search,
     test_list_setup,
     test_list_tear_down,
     MUNIT_TEST_OPTION_NONE,
     NULL
   },
   {
-    (char*) "/test_search_in_list",
-    test_search_in_list,
+    (char*) "/test_search_not_in_list",
+    test_search_not_in_list,
     test_list_setup,
     test_list_tear_down,
     MUNIT_TEST_OPTION_NONE,
     NULL
   },
+  
+  /* Usually this is written in a much more compact format; all these
+   * comments kind of ruin that, though.  Here is how you'll usually
+   * see entries written: */
+  { NULL, NULL, NULL, NULL, MUNIT_TEST_OPTION_NONE, NULL }
+};
+
+static MunitTest test_suite_graph_basic[] = {
   {
     (char*) "/test_read_n_m_from_file",
     test_read_n_m_from_file,
@@ -325,12 +318,8 @@ static MunitTest test_suite_tests[] = {
     MUNIT_TEST_OPTION_NONE,
     test_params_one
   },
-  /* Usually this is written in a much more compact format; all these
-   * comments kind of ruin that, though.  Here is how you'll usually
-   * see entries written: */
   { NULL, NULL, NULL, NULL, MUNIT_TEST_OPTION_NONE, NULL }
 };
-
 /* If you wanted to have your test suite run other test suites you
  * could declare an array of them.  Of course each sub-suite can
  * contain more suites, etc. */
@@ -341,15 +330,42 @@ static MunitTest test_suite_tests[] = {
 
 /* Now we'll actually declare the test suite.  You could do this in
  * the main function, or on the heap, or whatever you want. */
-static const MunitSuite test_suite = {
+static const MunitSuite test_suite_list = {
   /* This string will be prepended to all test names in this suite;
    * for example, "/example/rand" will become "/µnit/example/rand".
    * Note that, while it doesn't really matter for the top-level
    * suite, NULL signal the end of an array of tests; you should use
    * an empty string ("") instead. */
-  (char*) "test_suite_tests",
+  (char*) "test_list",
   /* The first parameter is the array of test suites. */
-  test_suite_tests,
+  test_list,
+  /* In addition to containing test cases, suites can contain other
+   * test suites.  This isn't necessary in this example, but it can be
+   * a great help to projects with lots of tests by making it easier
+   * to spread the tests across many files.  This is where you would
+   * put "other_suites" (which is commented out above). */
+  NULL,
+  /* An interesting feature of µnit is that it supports automatically
+   * running multiple iterations of the tests.  This is usually only
+   * interesting if you make use of the PRNG to randomize your tests
+   * cases a bit, or if you are doing performance testing and want to
+   * average multiple runs.  0 is an alias for 1. */
+  1,
+  /* Just like MUNIT_TEST_OPTION_NONE, you can provide
+   * MUNIT_SUITE_OPTION_NONE or 0 to use the default settings. */
+  MUNIT_SUITE_OPTION_NONE
+};
+
+
+static const MunitSuite test_suite_graph = {
+  /* This string will be prepended to all test names in this suite;
+   * for example, "/example/rand" will become "/µnit/example/rand".
+   * Note that, while it doesn't really matter for the top-level
+   * suite, NULL signal the end of an array of tests; you should use
+   * an empty string ("") instead. */
+  (char*) "test_suite_graph_basic",
+  /* The first parameter is the array of test suites. */
+  test_suite_graph_basic,
   /* In addition to containing test cases, suites can contain other
    * test suites.  This isn't necessary in this example, but it can be
    * a great help to projects with lots of tests by making it easier
@@ -377,5 +393,7 @@ int main(int argc, char* argv[MUNIT_ARRAY_PARAM(argc + 1)]) {
   /* Finally, we'll actually run our test suite!  That second argument
    * is the user_data parameter which will be passed either to the
    * test or (if provided) the fixture setup function. */
-  return munit_suite_main(&test_suite, NULL, argc, argv);
+  munit_suite_main(&test_suite_list, NULL, argc, argv);
+  //munit_suite_main(&test_suite_graph, NULL, argc, argv);
+  return 0;
 }
